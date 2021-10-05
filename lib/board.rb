3d05e23aac5b4070a3480eafa15d5298cdceee4b
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-10-05 21:27:52 +0700

    wrote the initally #drop

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,27 @@
 class Board
   attr_reader :grid
 
-  def initialize(grid:)
+  def initialize(grid: default_grid)
     @grid = grid
+  end
+
+  def drop(column:, marker:)
+    return nil if column_full?(column: column)
+
+    column
+  end
+
+  private
+
+  def default_grid
+    Array.new(7) { Array.new(6, '') }
+  end
+
+  def column_full?(column:)
+    grid_column(column: column).none?(&:nil?)
+  end
+
+  def grid_column(column:)
+    grid.transpose[column]
   end
 end
