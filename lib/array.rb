0647f7e3b1255a2adeff920b1e0207729cb49489
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-10-08 16:56:06 +0700

    linted

--- lib/array.rb
+++ lib/array.rb
@@ -2,7 +2,7 @@ class Array
   def four_items_same_value_continous?
     count_two_items_in_front_same_value = 0
     each_with_index do |item, index|
-      count_two_items_in_front_same_value +=1 if item && item == self[index + 1] && item == self[index + 2]
+      count_two_items_in_front_same_value += 1 if item && item == self[index + 1] && item == self[index + 2]
     end
     count_two_items_in_front_same_value == 2
   end
