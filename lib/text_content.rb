322c147668780ef271912e1c27374273aea6b3f9
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-10-12 12:23:19 +0700

    add module contained text for game

--- lib/text_content.rb
+++ lib/text_content.rb
@@ -1,26 +1,33 @@
 require_relative 'colorize'
 
 module TextContent
-  include Colorize
+  def green(string)
+    "\e[32;1m#{string}\e[0m"
+  end
 
-  def error(string)
-    red(string)
+  def red(string)
+    "\e[31;1m#{string}\e[0m"
   end
 
   def introduction
     intro = <<~MSG
-      This is a Connect Four game.
+      This is a Connect Four game. The board has 7 rows and 6 columns
       Rules:
-        * You can specilize your name and symbol at the start of the game
-        * When you can connect four pieces vertically, horizontally or diagonally you win
+        * Players can specilize their names and markers at the start of the game
+        * When a player can connect four pieces vertically, horizontally or diagonally he win
         * When the every cell of the board is filled, it is a tie
     MSG
     green(intro)
   end
 
-  def user_input_error(available_columns)
-    "Input error. Please enter a number amongs #{available_columns}"
+  def warning_message(message)
+    {
+      'empty_marker_error' => red('You have to put something!!'),
+      'column_error' => red("Input error. Please enter a number among available columns"),
+      'duplicate_error' => red('This character has been used. Please choose a different one'),
+      'long_marker_error' => red('Marker only contained 1 character. Please choose a different one')
+    }[message]
   end
 
-  module_function :user_input_error
+  module_function :warning_message, :red, :green
 end
